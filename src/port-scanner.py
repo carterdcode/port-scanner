bd4838ef1709d527745e8db1dc8dc3e2564b60c8
carterdcode <117289619+carterdcode@users.noreply.github.com>
2025-07-03 21:26:29 +0100

    Added number of threads parameter
    Allow specifying number of threads from the command line

--- src/port-scanner.py
+++ src/port-scanner.py
@@ -10,7 +10,7 @@ GREEN = Fore.GREEN
 RESET = Fore.RESET
 GRAY = Fore.LIGHTBLACK_EX
 
-N_THREADS = 100
+DEFAULT_N_THREADS = 100
 # thread queue
 q = Queue()
 print_lock = Lock()
@@ -28,7 +28,7 @@ def port_scan(host, port):
     finally:
         s.close()
 
-def scan_thread():
+def scan_thread(host):
     global q
     while True:
         # get port num from queue
@@ -38,10 +38,10 @@ def scan_thread():
         # tells queue the current port scan is done
         q.task_done()
 
-def main(host, ports):
+def main(host, ports, n_threads):
     global q
-    for t in range(N_THREADS):
-        t = Thread(target=scan_thread)
+    for t in range(n_threads):
+        t = Thread(target=scan_thread, args=(host,))
         # when daemon set to true, current thread ends when main thread ends
         t.daemon = True
         # start deamon thread
@@ -55,21 +55,39 @@ def main(host, ports):
 if len(sys.argv) == 2:
     host = sys.argv[1]
     start_port, end_port = 1, 65535
+    n_threads = DEFAULT_N_THREADS
 elif len(sys.argv) == 3:
     host = sys.argv[1]
     ports = sys.argv[2]
     start_port, end_port = ports.split("-")
     start_port, end_port = int(start_port), int(end_port)
+    n_threads = DEFAULT_N_THREADS
+elif len(sys.argv) == 4:
+    host = sys.argv[1]
+    ports = sys.argv[2]
+    start_port, end_port = ports.split("-")
+    start_port, end_port = int(start_port), int(end_port)
+    n_threads = int(sys.argv[3])
 else:
-    print(sys.argv)
-    args = input("Enter args in format HostIP StartPort-EndPort: ").split()
-    host = args[0]
-    if len(args) > 1:
-        start_port, end_port = args[1].split("-")
-        start_port, end_port = int(start_port), int(end_port)
-    else:
-        start_port, end_port = 1, 65535
+    while True:
+        args = input("Enter args in format HostIP StartPort-EndPort NumberOfThreads: ").split()
+        if len(args) < 1 or not args[0]:
+            print("Host IP is required. Please try again.")
+            continue
+        host = args[0]
+        try:
+            if len(args) >= 2:
+                start_port, end_port = args[1].split("-")
+                start_port, end_port = int(start_port), int(end_port)
+            else:
+                start_port, end_port = 1, 65535
+            if len(args) >= 3:
+                n_threads = int(args[2])
+            else:
+                n_threads = DEFAULT_N_THREADS
+            break  # Exit loop if everything is valid
+        except Exception as e:
+            print(f"Invalid input: {e}. Please try again.")
 
 ports = [ p for p in range(start_port, end_port)]
-
-main(host, ports)
+main(host, ports, n_threads)
